5616ececd30204ae36df581bfe99f34c449a2d20
GinoxXP <ginoxxp@yandex.ru>
2021-03-02 19:56:00 +0300

    Add player

---
+++ Assets/Scripts/DetectZone.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class DetectZone : MonoBehaviour
+{
+    public GameObject detectedObject;
+
+    void Start()
+    {
+        detectedObject = gameObject;
+    }
+
+    void OnTriggerStay2D(Collider2D collider)
+    {
+        if(collider.gameObject.TryGetComponent(out Rigidbody2D rb))
+            if(detectedObject != rb.gameObject)
+                detectedObject = rb.gameObject;
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if(collider.gameObject.TryGetComponent(out Rigidbody2D rb))
+            detectedObject = null;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speed;
+    private Vector3 target;
+
+    private Vector3 moveDirection;
+
+    [SerializeField] BoxCollider2D upZone;
+    [SerializeField] BoxCollider2D leftZone;
+    [SerializeField] BoxCollider2D downZone;
+    [SerializeField] BoxCollider2D rightZone;
+
+    private bool isRunning;
+
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        if(!isRunning && context.performed)
+        {
+            isRunning = true;
+
+            moveDirection = context.ReadValue<Vector2>();
+            SetTarget();
+            StartCoroutine(Move());
+        }
+    }
+
+    void SetTarget()
+    {
+        target = transform.position + moveDirection;
+    }
+
+    IEnumerator Move()
+    {
+        while (Vector3.Distance(transform.position, target) > float.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position,
+                                                    target,
+                                                    speed * Time.deltaTime);
+            yield return null;
+        }
+        isRunning = false;
+    }
+
+}
