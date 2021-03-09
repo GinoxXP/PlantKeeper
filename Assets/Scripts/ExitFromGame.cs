2537a86c0b8f19d4c508ef7799bff10efbd0fae9
GinoxXP <ginoxxp@yandex.ru>
2021-03-09 16:57:07 +0300

    Add main menu

---
+++ Assets/Scripts/ExitFromGame.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitFromGame : MonoBehaviour
+{
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/PauseButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseButton : MonoBehaviour
+{
+    [SerializeField] private GameObject target;
+
+    public void Click()
+    {
+        target.SetActive(!target.activeSelf);
+    }
+}
