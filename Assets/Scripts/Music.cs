c52251143d6ba89f97f4053f3d8d27fb3d489ae7
GinoxXP <ginoxxp@yandex.ru>
2021-03-09 23:29:41 +0300

    Add music

---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    void Start()
+    {
+        if(GameObject.FindGameObjectsWithTag("Music").Length == 1)
+            DontDestroyOnLoad(this.gameObject);
+        else
+            Destroy(gameObject);
+    }
+}
